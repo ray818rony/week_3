ce4890ae8a191a2cc29ff2000795083d6aead1ee
Rony Ray Shabo <79472386+ray818rony@users.noreply.github.com>
2021-06-04 01:39:52 -0500

    Modules
    learning how to create a new calculator module

---
+++ module2.py
@@ -0,0 +1,17 @@
+
+def area_calc():
+    length = int(input('Enter length: '))
+    width = int(input('Enter width: '))
+    area = length * width
+    print("the area is :", area)
+
+area_calc()
+
+from math import pi
+def circum_of_circle():
+    radius = int(input('Please enter the radius '))
+    circumference = 2 * pi * radius
+    print("the radius of this circle is :" , circumference)
+
+
+circum_of_circle()
